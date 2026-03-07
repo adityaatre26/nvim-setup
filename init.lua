b5852b3e8ce1ddbb9f0eec5679aff2c77852cadf
adityaatre26 <aditya.atre26@gmail.com>
2026-03-07 21:16:44 +0530

    Copilot removal, transparency setting

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require("josean.core.options")
 require("josean.core")
 require("josean.lazy")
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    local save = vim.fn.winsaveview()
+    vim.cmd([[silent! %s/\r//g]])
+    vim.fn.winrestview(save)
+  end,
+})
