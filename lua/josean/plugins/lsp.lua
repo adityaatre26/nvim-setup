6d430893a006d9ddbab82514444543ea4ae5b6c3
adityaatre26 <aditya.atre26@gmail.com>
2026-03-18 14:59:50 +0530

    Changes to lsp config

--- lua/josean/plugins/lsp.lua
+++ lua/josean/plugins/lsp.lua
@@ -20,31 +20,18 @@ return {
     },
     config = function()
       require("mason-lspconfig").setup({
-        -- LSP servers that mason will automatically install
         ensure_installed = {
-          -- Go
           "gopls",
-          -- JavaScript / TypeScript / React / Next.js
           "ts_ls",
-          -- HTML
           "html",
-          -- CSS
           "cssls",
-          -- Tailwind CSS
           "tailwindcss",
-          -- JSON
           "jsonls",
-          -- YAML
           "yamlls",
-          -- Dockerfile
           "dockerls",
-          -- Docker Compose
           "docker_compose_language_service",
-          -- Lua (for your Neovim config)
           "lua_ls",
-          -- ESLint
           "eslint",
-          -- Emmet (HTML/CSS abbreviations)
           "emmet_ls",
         },
       })
@@ -59,43 +46,10 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      local lspconfig = require("lspconfig")
       local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
       -- Enhanced capabilities from nvim-cmp (enables LSP-driven autocompletion)
       local capabilities = cmp_nvim_lsp.default_capabilities()
-
-      -- Keymaps that activate when an LSP attaches to a buffer
-      local on_attach = function(client, bufnr)
-        local opts = { buffer = bufnr, silent = true }
-
-        -- Go to declaration
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
-        -- Go to definition
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
-        -- Show hover info
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Show hover info" }))
-        -- Go to implementation
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
-        -- Show signature help
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Show signature help" }))
-        -- Go to type definition
-        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, vim.tbl_extend("force", opts, { desc = "Go to type definition" }))
-        -- Rename symbol
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
-        -- Code actions
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code actions" }))
-        -- Show references
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Show references" }))
-        -- Show diagnostics for line
-        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show line diagnostics" }))
-        -- Go to previous diagnostic
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Previous diagnostic" }))
-        -- Go to next diagnostic
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
-        -- Restart LSP
-        vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", vim.tbl_extend("force", opts, { desc = "Restart LSP" }))
-      end
 
       -- Diagnostic signs in the gutter
       local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
@@ -104,14 +58,35 @@ return {
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
       end
 
+      -- Keymaps that activate when an LSP attaches to a buffer
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf, silent = true }
+
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Show hover info" }))
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Show signature help" }))
+          vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, vim.tbl_extend("force", opts, { desc = "Go to type definition" }))
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code actions" }))
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Show references" }))
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show line diagnostics" }))
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Previous diagnostic" }))
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
+          vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", vim.tbl_extend("force", opts, { desc = "Restart LSP" }))
+        end,
+      })
+
       -- ═══════════════════════════════════════════
-      -- Server configurations
+      -- Server configurations (Neovim 0.11+ API)
       -- ═══════════════════════════════════════════
 
       -- Go
-      lspconfig.gopls.setup({
+      vim.lsp.config("gopls", {
         capabilities = capabilities,
-        on_attach = on_attach,
         settings = {
           gopls = {
             analyses = {
@@ -125,45 +100,38 @@ return {
       })
 
       -- TypeScript / JavaScript / React / Next.js
-      lspconfig.ts_ls.setup({
+      vim.lsp.config("ts_ls", {
         capabilities = capabilities,
-        on_attach = on_attach,
       })
 
       -- ESLint
-      lspconfig.eslint.setup({
+      vim.lsp.config("eslint", {
         capabilities = capabilities,
-        on_attach = on_attach,
       })
 
       -- HTML
-      lspconfig.html.setup({
+      vim.lsp.config("html", {
         capabilities = capabilities,
-        on_attach = on_attach,
       })
 
       -- CSS
-      lspconfig.cssls.setup({
+      vim.lsp.config("cssls", {
         capabilities = capabilities,
-        on_attach = on_attach,
       })
 
       -- Tailwind CSS
-      lspconfig.tailwindcss.setup({
+      vim.lsp.config("tailwindcss", {
         capabilities = capabilities,
-        on_attach = on_attach,
       })
 
       -- JSON
-      lspconfig.jsonls.setup({
+      vim.lsp.config("jsonls", {
         capabilities = capabilities,
-        on_attach = on_attach,
       })
 
       -- YAML
-      lspconfig.yamlls.setup({
+      vim.lsp.config("yamlls", {
         capabilities = capabilities,
-        on_attach = on_attach,
         settings = {
           yaml = {
             schemas = {
@@ -177,32 +145,28 @@ return {
       })
 
       -- Dockerfile
-      lspconfig.dockerls.setup({
+      vim.lsp.config("dockerls", {
         capabilities = capabilities,
-        on_attach = on_attach,
       })
 
       -- Docker Compose
-      lspconfig.docker_compose_language_service.setup({
+      vim.lsp.config("docker_compose_language_service", {
         capabilities = capabilities,
-        on_attach = on_attach,
       })
 
       -- Emmet
-      lspconfig.emmet_ls.setup({
+      vim.lsp.config("emmet_ls", {
         capabilities = capabilities,
-        on_attach = on_attach,
         filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
       })
 
       -- Lua (configured for Neovim development)
-      lspconfig.lua_ls.setup({
+      vim.lsp.config("lua_ls", {
         capabilities = capabilities,
-        on_attach = on_attach,
         settings = {
           Lua = {
             diagnostics = {
-              globals = { "vim" }, -- recognize 'vim' global
+              globals = { "vim" },
             },
             completion = {
               callSnippet = "Replace",
@@ -215,6 +179,22 @@ return {
             },
           },
         },
+      })
+
+      -- Enable all configured servers
+      vim.lsp.enable({
+        "gopls",
+        "ts_ls",
+        "eslint",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "jsonls",
+        "yamlls",
+        "dockerls",
+        "docker_compose_language_service",
+        "emmet_ls",
+        "lua_ls",
       })
     end,
   },
